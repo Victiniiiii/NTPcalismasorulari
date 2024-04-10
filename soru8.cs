c1b5c1de5dfa6ecf0dc5699206aa2a0e4bd2e820
Victiniiiii <138769411+Victiniiiii@users.noreply.github.com>
2024-04-10 17:00:50 +0300

    Update soru8.cs

--- soru8.cs
+++ soru8.cs
@@ -1,23 +1,30 @@
 // Klavyeden girilen sayı n olsun. n! i hesaplayan programı yazınız.
 
+using System.Numerics;
+
 class Program
 {
     static void Main()
     {
-        int faktoriyel = 1;
-        Console.WriteLine("Bir sayı giriniz");
+        BigInteger faktoriyel = 1;
+        Console.WriteLine("Bir sayı giriniz:");
         string input = Console.ReadLine();
         int n = Convert.ToInt32(input);
 
-        for (int i = n; i>=1; i--)
+        for (int i = n; i >= 1; i--)
         {
             faktoriyel *= i;
         }
 
-        Console.WriteLine("faktoriyel sonucu: "+faktoriyel);
+        Console.WriteLine("Faktöriyel sonucu: " + faktoriyel);
     }
 }
+
 
 // Bir sayi giriniz
 // 6
 // faktoriyel sonucu: 720
+
+// Bir sayi giriniz:
+// 31
+// Faktöriyel sonucu: 8222838654177922817725562880000000
