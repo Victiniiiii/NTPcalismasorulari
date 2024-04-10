7796d6d63aa19fc99c9eebee4e78b76673d14b45
Victiniiiii <138769411+Victiniiiii@users.noreply.github.com>
2024-04-10 13:25:57 +0300

    Create soru7.cs

---
+++ soru7.cs
@@ -0,0 +1,16 @@
+class Program
+{
+    static void Main()
+    {
+        int faktoriyel = 1;
+
+        for (int i = 8; i>=1; i--)
+        {
+            faktoriyel *= i;
+        }
+
+        Console.WriteLine("faktoriyel sonucu: "+faktoriyel);
+    }
+}
+
+// faktoriyel sonucu: 40320
