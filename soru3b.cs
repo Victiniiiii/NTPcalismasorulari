f7b0f38e5d844efa3707db525d3f2cdfacd027ce
Victiniiiii <138769411+Victiniiiii@users.noreply.github.com>
2024-04-10 13:22:06 +0300

    Create soru3b.cs

---
+++ soru3b.cs
@@ -0,0 +1,42 @@
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("İlk sayıyı giriniz.");
+        string sayibir = Console.ReadLine();
+        int n1 = Convert.ToInt32(sayibir);
+
+        Console.WriteLine("İkinci sayıyı giriniz.");
+        string sayiiki = Console.ReadLine();
+        int n2 = Convert.ToInt32(sayiiki);
+
+        Console.WriteLine("Üçüncü sayıyı giriniz.");
+        string sayiuc = Console.ReadLine();
+        int n3 = Convert.ToInt32(sayiuc);
+
+        Console.WriteLine("Dördüncü sayıyı giriniz.");
+        string sayidort = Console.ReadLine();
+        int n4 = Convert.ToInt32(sayidort);
+
+        Console.WriteLine("Beşinci sayıyı giriniz.");
+        string sayibes = Console.ReadLine();
+        int n5 = Convert.ToInt32(sayibes);
+
+        int toplam = n1 + n2 + n3 + n4 + n5;
+        int ortalama = toplam / 5;
+
+        Console.WriteLine("Girdiğiniz 5 sayının toplamı: " + toplam + " ortalamaları ise " + ortalama);
+    }
+}
+
+// İlk sayıyı giriniz.
+// 1
+// İkinci sayıyı giriniz.
+// 2
+// Üçüncü sayıyı giriniz.
+// 3
+// Dördüncü sayıyı giriniz.
+// 4
+// Beşinci sayıyı giriniz.
+// 5
+// Girdiğiniz 5 sayının toplamı: 15 ortalamaları ise 3
