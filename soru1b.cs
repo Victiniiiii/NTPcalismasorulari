d8afcdfc7fad84f62062cca0da86311d280d4f1d
Victiniiiii <138769411+Victiniiiii@users.noreply.github.com>
2024-04-10 13:19:43 +0300

    Create soru1b.cs

---
+++ soru1b.cs
@@ -0,0 +1,16 @@
+class Program
+{
+    static void Main()
+    {
+        int toplam = 0;
+
+        for (int i = 10; i >= 1; i--)
+        {
+            toplam += i;
+        }
+
+        Console.WriteLine("10'dan 1'e kadar olan sayıların toplamı: " + toplam);
+    }
+}
+
+// 10'dan 1'e kadar olan sayıların toplamı: 55
