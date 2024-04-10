e461644fc9abc849bc63cd885244d19e3b98b25c
Victiniiiii <138769411+Victiniiiii@users.noreply.github.com>
2024-04-10 13:25:37 +0300

    Create soru6.cs

---
+++ soru6.cs
@@ -0,0 +1,41 @@
+class Program
+{
+    static void Main()
+    {
+        int toplam = 0;
+        int adet = 0;
+
+        while (true) {
+            Console.WriteLine("Notu giriniz veya çıkmak için DUR");
+            string input = Console.ReadLine();
+            if (input=="DUR")
+            {
+                break;
+            }
+            else{
+                int not = Convert.ToInt32(input);
+                toplam += not;
+                adet++;
+            }
+        }
+        
+        int ortalama = toplam / adet;
+        Console.WriteLine("Notların ortalaması "+ortalama);
+    }
+}
+
+
+
+// Notu giriniz veya çikmak için DUR
+// 4
+// Notu giriniz veya çikmak için DUR
+// 8
+// Notu giriniz veya çikmak için DUR
+// 3
+// Notu giriniz veya çikmak için DUR
+// 2
+// Notu giriniz veya çikmak için DUR
+// 7
+// Notu giriniz veya çikmak için DUR
+// DUR
+// Notlarin ortalamasi 4
