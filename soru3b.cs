cae79f4f06cb4c1914ef9db57320ee4dd1fa2edc
Victiniiiii <138769411+Victiniiiii@users.noreply.github.com>
2024-04-16 14:35:56 +0300

    Update soru3b.cs

--- soru3b.cs
+++ soru3b.cs
@@ -1,44 +1,31 @@
-// ortalama?
-
+// Klavyeden girilen 5 sayının toplamını hesaplayan C# programını yazınız.
+using System; // online compiler kullanıyorsanız using System yazmalısınız
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("İlk sayıyı giriniz.");
-        string sayibir = Console.ReadLine();
-        int n1 = Convert.ToInt32(sayibir);
-
-        Console.WriteLine("İkinci sayıyı giriniz.");
-        string sayiiki = Console.ReadLine();
-        int n2 = Convert.ToInt32(sayiiki);
-
-        Console.WriteLine("Üçüncü sayıyı giriniz.");
-        string sayiuc = Console.ReadLine();
-        int n3 = Convert.ToInt32(sayiuc);
-
-        Console.WriteLine("Dördüncü sayıyı giriniz.");
-        string sayidort = Console.ReadLine();
-        int n4 = Convert.ToInt32(sayidort);
-
-        Console.WriteLine("Beşinci sayıyı giriniz.");
-        string sayibes = Console.ReadLine();
-        int n5 = Convert.ToInt32(sayibes);
-
-        int toplam = n1 + n2 + n3 + n4 + n5;
+        int toplam = 0;
+        
+        for ( int i = 0 ; i<5 ; i++ ) {
+            Console.WriteLine("Sayı giriniz");
+            string input = Console.ReadLine();
+            int sayi = Convert.ToInt32(input);
+            toplam += sayi;
+        }
+        
         int ortalama = toplam / 5;
-
-        Console.WriteLine("Girdiğiniz 5 sayının toplamı: " + toplam + " ortalamaları ise " + ortalama);
+        Console.WriteLine("Girdiğiniz 5 sayının toplamı: " + toplam + " ortalamaları ise "+ ortalama);
     }
 }
 
-// İlk sayıyı giriniz.
+// Sayı giriniz
 // 1
-// İkinci sayıyı giriniz.
+// Sayı giriniz
 // 2
-// Üçüncü sayıyı giriniz.
+// Sayı giriniz
 // 3
-// Dördüncü sayıyı giriniz.
+// Sayı giriniz
 // 4
-// Beşinci sayıyı giriniz.
+// Sayı giriniz
 // 5
 // Girdiğiniz 5 sayının toplamı: 15 ortalamaları ise 3
