80481cbf77165182f318657aad8bdcb0d9d73371
Victiniiiii <138769411+Victiniiiii@users.noreply.github.com>
2024-05-23 23:16:29 +0300

    Create 2a.cs

---
+++ final/2a.cs
@@ -0,0 +1,37 @@
+/* 2. Bir sınıftaki 150 öğrencinin vize, ödev ve final notları verilsin.
+Geçme notu gn= (v*0.7+ö*0.3)*0.4 + f*0.6 olarak hesaplansın.
+    a. En yüksek gn alan öğrenciyi bulun. */
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        int[] vizeler = new int[5];
+        int[] odevler = new int[5];
+        int[] finaller = new int[5];
+        double[] genelnotlar = new double[5];
+        double maxnot = 0;
+        int maxogrenci = 0;
+
+        for (int i = 0; i < 5; i++)  
+        {
+            Console.WriteLine("Öğrencinin vize notunu giriniz:");
+            vizeler[i] = Convert.ToInt32(Console.ReadLine());    
+            Console.WriteLine("Öğrencinin ödev notunu giriniz:");
+            odevler[i] = Convert.ToInt32(Console.ReadLine());    
+            Console.WriteLine("Öğrencinin final notunu giriniz:");
+            finaller[i] = Convert.ToInt32(Console.ReadLine());   
+            genelnotlar[i] = vizeler[i] * 0.28 + odevler[i] * 0.12 + finaller[i] * 0.6;
+
+            if (genelnotlar[i] > maxnot) {
+                maxnot = genelnotlar[i];
+                maxogrenci = i;
+            }
+        }
+
+        Console.WriteLine("En yüksek genel not alan öğrenci " +maxnot + " ortalama ile " +maxogrenci+ ". sıradaki öğrenci");
+    }
+}
+
+// En yüksek genel not alan ögrenci 90 ortalama ile 3. siradaki ögrenci
