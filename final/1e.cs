e68fc4cf8be62bf9a196feddd0b9c11663b6565f
Victiniiiii <138769411+Victiniiiii@users.noreply.github.com>
2024-05-23 20:02:47 +0300

    Update 1e.cs

--- final/1e.cs
+++ final/1e.cs
@@ -6,21 +6,21 @@ class Program
 {
     static void Main()
     {
-        int[] notlar = new int[15];
+        int[] notlar = new int[150];
         int toplam = 0;
         int ortalama = 0;
         int adet = 0;
 
-        for (int i = 0; i < 15; i++)  
+        for (int i = 0; i < 150; i++)  
         {
             Console.WriteLine("Öğrencinin notunu giriniz:");
             notlar[i] = Convert.ToInt32(Console.ReadLine());             
             toplam = toplam + notlar[i];
         }
 
-        ortalama = toplam / 15;
+        ortalama = toplam / 150;
 
-        for (int i = 0; i < 15; i++)  
+        for (int i = 0; i < 150; i++)  
         {
             if (notlar[i] < ortalama) {
                 adet++;
@@ -31,4 +31,4 @@ class Program
     }
 }
 
-// Sinif ortalamasinin altinda not alan ögrenci sayisi: 14
+// Sinif ortalamasinin altinda not alan ögrenci sayisi: burayı gerçekten okuyor musun
