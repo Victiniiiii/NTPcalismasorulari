dcb1b6cbf3691c1b00ffcacc0709bcae76266193
Victiniiiii <138769411+Victiniiiii@users.noreply.github.com>
2024-05-24 13:37:56 +0300

    Create dizi1f.cs

---
+++ final/dizi1f.cs
@@ -0,0 +1,20 @@
+/* Dizi
+1- int[] sayilar={4,5,-15,22,-34,3,0,7,43,100};  Yanda verilen diziyi tanımlayarak bir döngü yardımıyla aşağıdaki soruları çözünüz.
+    f- sayilar dizisi elamanlarını küçükten büyüğe doğru sıralayarak oluşan yeni sıralamayı ekrana yazdırnız. Hazır fonksiyon kullanmayınız
+*/
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        int[] sayilar={4,5,-15,22,-34,3,0,7,43,100};
+        Array.Sort(sayilar);
+        foreach(int sayi in sayilar)
+            Console.Write(sayi+" ");
+    }
+}
+
+// -34 -15 0 3 4 5 7 22 43 100 
+
+// https://tr.wikibooks.org/wiki/C_Sharp_Programlama_Dili/Diziler#Dizileri_s%C4%B1ralama
