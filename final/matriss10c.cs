647d590575f47c09eca75502dfd75913027b7e77
Victiniiiii <138769411+Victiniiiii@users.noreply.github.com>
2024-05-25 18:23:33 +0300

    Create matriss10c.cs

---
+++ final/matriss10c.cs
@@ -0,0 +1,13 @@
+/* Matris
+    10. 4x5 lik bir A matrisi giriliyor.
+    c) Matrisin her bir sütunundaki elemanların enbüyüğünü ve hangi satır sütun indisine sahip olduğunu bulup yazdıran programı yazınız.
+*/
+
+using System;
+class Program
+{
+    static void Main()
+    {        
+        // bu soruyu pas geçiyorum, matrise tam olmayan sayı atamayı bile bilmiyorum 
+    }
+}
