4b65bc4cba328c40b01bc9354479f179eaadd488
Victiniiiii <138769411+Victiniiiii@users.noreply.github.com>
2024-05-25 19:04:24 +0300

    Create matriss11d.cs

---
+++ final/matriss11d.cs
@@ -0,0 +1,14 @@
+/* Matris
+    11. 5x5 lik bir A matrisi giriliyor
+    d) Matrisin son halini matris gibi yazdıran bir program yazınız
+*/
+
+using System;
+class Program
+{
+    static void Main()
+    {        
+        // ne ?
+    }
+}
+
