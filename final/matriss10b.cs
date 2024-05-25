e36a49e68f9d2880231043e9c12cb06053a1391a
Victiniiiii <138769411+Victiniiiii@users.noreply.github.com>
2024-05-25 17:27:40 +0300

    Create matriss10b.cs

---
+++ final/matriss10b.cs
@@ -0,0 +1,13 @@
+/* Matris
+    10. 4x5 lik bir A matrisi giriliyor.
+    b) Matrisin her bir satırındaki tamsayı olan elemanlarının çarpımını
+*/
+
+using System;
+class Program
+{
+    static void Main()
+    {        
+        // bu soruyu pas geçiyorum, matrise tam olmayan sayı atamayı bile bilmiyorum 
+    }
+}
